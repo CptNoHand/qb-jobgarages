59a10de647e4105196bf536117c23a03f3c9a9f5
CptNoHand <B_Marutschke@gmx.de>
2021-10-13 15:41:47 +0200

    added garages
    just added some garages, that I need on my server.

--- config.lua
+++ config.lua
@@ -8,12 +8,26 @@ Garages = {
         putVehicle = vector3(276.69, -339.85, 44.91),
         job = "dealership"
     },
-    ["template"] = {
-        label = "template",
-        takeVehicle = vector3(x, y, z),
-        spawnPoint = vector3(x, y, z),
-        putVehicle = vector3(x, y, z),
-        job = "none"
+    ["Gebrauchtwagenhändler"] = {
+        label = "Gebrauchtwagenhändler Garage",
+        takeVehicle = vector3(-355.58, -920.42, 31.08),
+        spawnPoint = vector3(-355.48, -910.02, 31.08),
+        putVehicle = vector3(-356.18, -897.53, 31.07),
+        job = "dealership"
+    },
+    ["sunrise"] = {
+        label = "Sunrise Autos",
+        takeVehicle = vector3(-767.8, -207.23, 37.28),
+        spawnPoint = vector4(-771.1, -212.39, 37.28, 208.3),
+        putVehicle = vector3(-771.1, -212.39, 37.28),
+        job = "sunrise"
+    },
+    ["Tuner"] = {
+        label = "Tunertreffgarage",
+        takeVehicle = vector3(152.92, -3092.49, 5.9),
+        spawnPoint = vector3(135.85, -3088.99, 5.9),
+        putVehicle = vector3(139.56, -3096.63, 5.9),
+        job = "tuner"
     },
 }
 
